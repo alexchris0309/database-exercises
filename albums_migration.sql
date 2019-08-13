99430a7bc1032c7862ca3d9756e1ee294269fc54
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-08-13 14:58:57 -0500

    albums sql exercise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXIST albums;
+
+CREATE TABLE albums (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(50) ,
+name VARCHAR (50),
+release_date DATE ,
+sales FLOAT,
+genre VARCHAR (100),
+PRIMARY KEY (id)
+);
